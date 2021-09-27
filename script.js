a617839378810ce3b20897b372e408a260e5d420
Afsal Rahman <afsalrahman539@gmail.com>
2021-09-27 23:36:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+ function playVideo1 (){
+
+   console.log("clicked");
+    
+       
+        document.getElementById("temp-img1").style.display="none"
+        document.getElementById("vid1").style.display="block"
+
+}
+function playVideo2 (){
+
+    console.log("clicked");
+     
+        
+         document.getElementById("temp-img2").style.display="none"
+         document.getElementById("vid2").style.display="block"
+ 
+ }
+ function playVideo3 (){
+
+    console.log("clicked");
+     
+        
+         document.getElementById("temp-img3").style.display="none"
+         document.getElementById("vid3").style.display="block"
+ 
+ }
+ function playVideo4 (){
+
+    console.log("clicked");
+     
+        
+         document.getElementById("temp-img4").style.display="none"
+         document.getElementById("vid4").style.display="block"
+ 
+ }
+ function playVideo5 (){
+
+    console.log("clicked");
+     
+        
+         document.getElementById("temp-img5").style.display="none"
+         document.getElementById("vid5").style.display="block"
+ 
+ }
